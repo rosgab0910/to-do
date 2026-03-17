afbcaafa3224f4554879c5b9f59473a7191c968c
rosgab0910 <gabrieloliveirarosa@colegiopaulodetarso.com.br>
2026-03-17 14:48:15 +0000

    js

---
+++ script.js
@@ -0,0 +1,7 @@
+const taskBtn = document.querySelector("#taskBtn")
+const taskList = document.querySelector("#taskList")
+const taskInput = document.querySelector("#taskInput")
+
+taskBtn.addEventListener("click", function(){
+    alert("CLIQUEI!!!")
+})
